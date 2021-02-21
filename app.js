389c5e74de10b2e4d743c0926891f94ae2dbf07d
pandoragear <andy.dang.it@gmail.com>
2021-02-21 01:23:20 -0600

    Add Fetch from openweathermap.org

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ var inputValue = document.querySelector(".inputValue");
 var name = document.querySelector(".name");
 var desc = document.querySelector(".desc");
 var temp = document.querySelector(".temp");
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=c4c17f7487428c23c01090220e623f3b')
+. then(response => response.json())
+.then(data => console.log(data))
